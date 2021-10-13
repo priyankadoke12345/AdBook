2f72f4f2ebd2fd04eb9a39ec906a94f316275eef
Priyankadoke12345 <priyankaadoke@gmail.com>
2021-10-13 10:00:57 +0530

    New Contacts to adress Book done successfully

--- src/AdBook/AddressBook.java
+++ src/AdBook/AddressBook.java
@@ -43,6 +43,36 @@ public class AddressBook {
         arrayList.add(new AllContacts(fname, lname, address, city, state, zip, phone, email));
         System.out.println("Data added Successfully");
     }
+    public void NewEdit(String name){
+        for (int i=0;i<arrayList.size();i++){
+
+            if(arrayList.get(i).getFirstName()==name){
+                System.out.println("Enter  the new  details");
+                System.out.println("First Name");
+                String fname = scanner.nextLine();
+                System.out.println("Last Name");
+                String lname = scanner.nextLine();
+                System.out.println("Address");
+                String address = scanner.nextLine();
+                System.out.println("City");
+                String city = scanner.nextLine();
+                System.out.println("state");
+                String state = scanner.nextLine();
+                System.out.println("zip");
+                String zip = scanner.nextLine();
+                System.out.println("PhoneNumber");
+                String phone = scanner.nextLine();
+                System.out.println("Email");
+                String email = scanner.nextLine();
+                arrayList.set(i,new AllContacts(fname,lname,address,city,state,zip,phone,email));
+                System.out.println("Data updated successfully");
+                break;
+            }
+        }
+
+
+
+    }
 
 
 }
